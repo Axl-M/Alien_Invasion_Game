07e523a6a67f3504dbf4ac9089d7fb2a81b07130
Alexander M <axlmalyasov@gmail.com>
2022-05-17 17:54:35 +1000

    settings separated in file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,13 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
-    # инициализирует игру и создает объект экрана
+    # инициализирует pygame, settings и  объект экрана
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # цвет фона
     bg_color = (230, 230, 230)
@@ -18,7 +21,7 @@ def run_game():
                 sys.exit()
 
         # при каждом проходе цикла перерисовать экран
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # отображение оследнего прорисованного экрана
         pygame.display.flip()

189a47ccb03898672caf7f2fca22b7a8dab96bef
Alexander M <axlmalyasov@gmail.com>
2022-05-17 22:10:38 +1000

    Рефакторинг check_events()

--- game_functions.py
+++ game_functions.py
@@ -2,22 +2,31 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """ реагирует на нажатие клавиш """
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """ реагирует на отпускание клавиш """
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
-    """ обрабатывает нажатия клавишь и события мыши """
+    """ обрабатывает нажатия клавиш и события мыши """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            # переместить корабль вправо
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):

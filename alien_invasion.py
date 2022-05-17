27cd9e360aa995ba529601cdce94f9bd93fcae39
Alexander M <axlmalyasov@gmail.com>
2022-05-17 21:03:57 +1000

    ship moves constantly while key RIGHT pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ def run_game():
     # основной цикл игры
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
--- ship.py
+++ ship.py
@@ -14,6 +14,16 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # флаг перемещения
+        self.moving_right = False
+
+
+    def update(self):
+        """ обновляет позицию корабля с учетом флага """
+        if self.moving_right:
+            self.rect.centerx += 1
+
+
     def blitme(self):
         """ Рисует корабль в текущей позиции """
         self.screen.blit(self.image, self.rect)

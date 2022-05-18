796bd473718744e539fb8da33226d8b0dd1cb599
Alexander M <axlmalyasov@gmail.com>
2022-05-18 21:00:32 +1000

    moved fire code to separate function

--- game_functions.py
+++ game_functions.py
@@ -13,11 +13,15 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Создание новой пули и включение ее в группу bullets.
-        if len(bullets) < ai_settings.bulets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """ Выпускает пулю если максимум еще не достигнут  """
+    # Создание новой пули и включение ее в группу bullets.
+    if len(bullets) < ai_settings.bulets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -50,6 +54,7 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     # отображение последнего прорисованного экрана
     pygame.display.flip()
+
 
 def update_bullets(bullets):
     """ Обновляет позиции пуль и удаляет старые пули """

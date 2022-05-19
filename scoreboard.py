05e463aadd7455b7054066805d7e9862da454d49
Alexander M <axlmalyasov@gmail.com>
2022-05-19 21:57:15 +1000

    printout score

--- scoreboard.py
+++ scoreboard.py
@@ -14,3 +14,17 @@ class Scoreboard:
         self.font = pygame.font.SysFont(None, 48)
         #  Подготовка исходного изображения.
         self.prep_score()
+
+    def prep_score(self):
+        """ Преобразует текущий счет в графическое изображение. """
+        score_str = str(self.stats.score)
+        self.score_img = self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)
+
+        # Вывод счета в правой верхней части экрана. И расширяется влево с ростом значения и ширины числа
+        self.score_rect = self.score_img.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20  # смещаем его правую сторону на 20 пикселов от правого края экрана
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """ Выводит счет на экран. """
+        self.screen.blit(self.score_image, self.score_rect)
